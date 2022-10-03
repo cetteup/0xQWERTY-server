be38890c3583f8f758fcc5a962edf315db954f78
cetteup <me@cetteup.com>
2022-10-03 14:04:34 +0200

    fix: Correct spelling error in self base uri

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default abstract class Config {
     static readonly LISTEN_PORT: number = Number(process.env.PORT || 8080);
     static readonly LOG_LEVEL: string = process.env.LOG_LEVEL || 'info';
-    static readonly SELF_BASE_URI: string = process.env.SELF_BASE_URI || 'ttps://api.0xqwerty.com';
+    static readonly SELF_BASE_URI: string = process.env.SELF_BASE_URI || 'https://api.0xqwerty.com';
     static readonly CLIENT_ID: string = process.env.CLIENT_ID || '';
     static readonly APP_ACCESS_TOKEN: string = process.env.APP_ACCESS_TOKEN || '';
     static readonly EVENTSUB_SECRET: string = process.env.EVENTSUB_SECRET || '';
