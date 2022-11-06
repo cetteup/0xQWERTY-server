b61d5265852bb9a747114d70a3816d43eb40a384
cetteup <me@cetteup.com>
2022-11-06 22:56:03 +0100

    feat: Refresh chatbot oauth access token

--- src/bot.ts
+++ src/bot.ts
@@ -2,26 +2,38 @@ import * as socketio from 'socket.io-client';
 import * as tmi from 'tmi.js';
 import Config from './config';
 import { logger } from './logger';
+import axios from 'axios';
 
 logger.info('Connecting to socket.io server:', Config.SOCKETIO_SERVER_ADDR);
 const io = socketio.default(Config.SOCKETIO_SERVER_ADDR);
 
 const chatbotChannels = Config.CHATBOT_CHANNELS.split(' ');
+const oauthTokens: {
+    access: string
+    refresh?: string
+} = {
+    access: Config.CHATBOT_ACCESS_TOKEN,
+    refresh: Config.CHATBOT_REFRESH_TOKEN
+};
 
 const client = new tmi.Client({
     connection: { reconnect: true },
     identity: {
         username: '0xqwerty',
-        password: `oauth:${Config.CHATBOT_OAUTH_TOKEN}`
+        password: getClientPassword
     },
-    channels: chatbotChannels.slice()
+    channels: chatbotChannels.slice(),
+    options: {
+        messagesLogLevel: 'debug'
+    },
+    logger: logger.getChildLogger({ name: 'tmiLogger' })
 });
 
 client.connect();
 
-// Join all channels we are supposed to annouce redemptions on
+// Join all channels we are supposed to announce redemptions on
 for (const channel of chatbotChannels) {
-    logger.info('Joining Twitch chat of channel:', channel);
+    logger.info('Joining redemption announcement room for:', channel);
     io.emit('join', `streamer:${channel}`);
 }
 
@@ -45,3 +57,64 @@ io.on('redemption', (data: Redemption) => {
 io.on('message', (msg: string) => {
     logger.info('Received Socket.IO message:', msg);
 });
+
+type TwitchTokenResponse = {
+    access_token: string
+    expires_in: number
+    refresh_token: string
+    scope: string[]
+    token_type: string
+}
+
+async function getClientPassword(): Promise<string> {
+    // Return current access token if refresh is not possible or access token is still valid
+    if (!Config.CLIENT_SECRET || !oauthTokens.refresh || await isAccessTokenValid(oauthTokens.access)) {
+        return formatOAuthPassword(oauthTokens.access);
+    }
+
+    const params = new URLSearchParams({
+        client_id: Config.CLIENT_ID,
+        client_secret: Config.CLIENT_SECRET,
+        grant_type: 'refresh_token',
+        refresh_token: oauthTokens.refresh
+    });
+
+    try {
+        const resp = await axios.post('https://id.twitch.tv/oauth2/token', params, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        const data: TwitchTokenResponse = resp.data;
+        oauthTokens.access = data.access_token;
+        oauthTokens.refresh = data.refresh_token;
+        logger.debug('Successfully refreshed chatbot access token');
+    }
+    catch (e: any) {
+        logger.error('Failed to refresh chatbot access token:', e.message);
+    }
+
+    // We can't really handle any errors during the refresh (e.g. Twitch offline, refresh token invalid),
+    // so just return the current access token (refreshed or not)
+    return formatOAuthPassword(oauthTokens.access);
+}
+
+export async function isAccessTokenValid(accessToken: string): Promise<boolean> {
+    try {
+        await axios.get('https://api.twitch.tv/helix/users', {
+            headers: {
+                'Client-Id': Config.CLIENT_ID,
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
+}
+
+export function formatOAuthPassword(accessToken: string) {
+    return `oauth:${accessToken}`;
+}
+
--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,11 @@ export default abstract class Config {
     static readonly LOG_LEVEL: string = process.env.LOG_LEVEL || 'info';
     static readonly SELF_BASE_URI: string = process.env.SELF_BASE_URI || 'https://0xqwerty-api.cetteup.com';
     static readonly CLIENT_ID: string = process.env.CLIENT_ID || '';
+    static readonly CLIENT_SECRET?: string = process.env.CLIENT_SECRET;
     static readonly APP_ACCESS_TOKEN: string = process.env.APP_ACCESS_TOKEN || '';
     static readonly EVENTSUB_SECRET: string = process.env.EVENTSUB_SECRET || '';
     static readonly SOCKETIO_SERVER_ADDR: string = process.env.SOCKETIO_SERVER_ADDR || '';
-    static readonly CHATBOT_OAUTH_TOKEN: string = process.env.CHATBOT_OAUTH_TOKEN || '';
+    static readonly CHATBOT_ACCESS_TOKEN: string = process.env.CHATBOT_ACCESS_TOKEN || '';
+    static readonly CHATBOT_REFRESH_TOKEN?: string = process.env.CHATBOT_REFRESH_TOKEN;
     static readonly CHATBOT_CHANNELS: string = process.env.CHATBOT_CHANNELS || '';
 }
